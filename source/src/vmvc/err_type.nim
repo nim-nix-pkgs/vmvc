33b791e9c7f11e12a097067fe3a8df26b2d3788d
hugosenari <hugosenari@gmail.com>
2022-09-03 19:58:21 -0300

    feat(nimpkgs): update lock

--- source/src/vmvc/err_type.nim
+++
@@ -1,5 +0,0 @@
-type ErrorType* {.pure.} = enum
-  None, Uninitialized, Parse, Validation, IO, UnknownCommand, UnknownSubCommand,
-      UnknownConfigKey, BadValue, ValueNotInRange, MissingKey, MissingConfig,
-          Other, NotImplemented
-
